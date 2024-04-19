7f7e2cf63d9d21313a4030b4046d7d23ab31d96b
Lejla <lejladzanko@gmail.com>
2024-04-19 08:17:34 +0200

    db connection created

--- src/index.js
+++ src/index.js
@@ -9,6 +9,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+async function getDBConnection(){
+    const connection = await mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: "library_database"
+    });
+    connection.connect();
+    return connection;
+}
+
 const port = process.env.PORT || 5001;
 app.listen(port, () => {
     console.log(`Server is listening at port: ${port}`);
